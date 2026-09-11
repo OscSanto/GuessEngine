1f7bf4f71f3dac9d6a1af7c0888246e76fdc6736
OscSanto <108152800+OscSanto@users.noreply.github.com>
2026-09-10 23:55:14 -0500

    REMOVED: hardwired endpoints, and replaced with routes to 'guesses, players,topics,votes'

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,31 @@
+import 'dotenv/config';
 import express from 'express'; 
 import type { Request, Response, Application } from 'express';
+import {db} from './prisma/db';
+import { topicRouter } from './routes/topics';
+import { guessesRouter } from './routes/guesses';
+import { playerRouter } from './routes/players';
+import { voteRouter } from './routes/votes';
 
+//Split routes out of index.ts
 
 const app = express();
-const port: number = 4000;
+const port = Number(process.env['PORT']) || 4000;
+app.use(express.json()); 
+app.get('/health', (_req: Request, res: Response)=>{ res.status(200).json({status: 'ok!'}); });
 
-app.use(express.json());
-
-
-app.get('/', (request: Request, response: Response) => { 
-    console.log("getRequest");
-    response.status(200).json({
-        message: "hello world! Typescript with express is working!"
-    })
-});//how do we get/check/handle the request package? 
+//app.get(path,handler)
+app.use('/topic', topicRouter);
+app.use('/guesses', guessesRouter);
+app.use('/votes', voteRouter);
+app.use('/players', playerRouter);
 
 
+
+
+
+
+//app.lsten() goes last. Always
 //are we just listening? not doing anything else?
 //is this like "/health"? where we check to see if the server is avaiable?
 app.listen(port, () => {
